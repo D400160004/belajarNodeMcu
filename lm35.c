c32f91d4f271455981d567824706903900e0ff01
justadnan <adnanmuh0@gmail.com>
2018-11-20 02:25:06 +0700

    Create lm35.c

---
+++ lm35.c
@@ -0,0 +1,17 @@
+int PinSuhu = A0 ;
+float suhu, data ;
+void setup() {
+Serial.begin(9600) ;//BOUDRATE
+pinMode(PinSuhu, INPUT) ;
+}
+void loop() {
+data = analogRead(PinSuhu) ;
+suhu = data * 500 / 1024 ;
+Serial.print("data : ") ;
+Serial.print(data ) ;
+Serial.print(" Suhu : ");
+Serial.print(suhu) ;
+Serial.print(" C") ;
+Serial.println();
+delay(1000) ;
+}
